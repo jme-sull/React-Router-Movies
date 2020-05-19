dab065fa1e809cd811cbcb4f3c2e74c40dee2bc6
Jamie Sullivan <jamiesullivan@jamies-mbp.lan>
2020-05-19 07:27:17 -0400

    mvp

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { Route, Switch, Link } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 
 //components
 import SavedList from './Movies/SavedList';
